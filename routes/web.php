83da5820d06015d4667f393f02a879105aadbde8
Kristofers Rozevskis <kristofers.rozevskis@gmail.com>
2024-05-29 23:01:02 +0300

    Added ability to search playlist names

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
